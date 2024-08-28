4f03a0becb5653f5a5849eeb754cbdce042360e1
Álvaro Bátrez <alvaro.batrez@machinecare.com.mx>
2024-08-28 17:57:01 -0600

    adapter

--- structural_patterns/adapter/main.cpp
+++ structural_patterns/adapter/main.cpp
@@ -1,4 +1,31 @@
 #include <iostream>
+#include <cmath>
+
+// Target: Interfaz esperada por el cliente
+class DrawingAPI
+{
+    public:
+
+    virtual void drawCircle(double x, double y, double radius) = 0;
+    virtual void drawSquare(double x, double y, double side) = 0;
+    virtual ~DrawingAPI() = default;
+};
+
+// Adaptee: La clase con una interfaz incompatible
+class LegacyDrawingAPI
+{
+    public:
+
+    void drawLegacyCircle(double x, double y, double radius)
+    {
+        std::cout << "Drawing Circle in Legacy API at (" << x << ", " << y << ") with radius " << radius << std::endl;
+    }
+
+    void drawLegacySquare(double x, double y, double side)
+    {
+        std::cout << "Drawing Square in Legacy API at (" << x << ", " << y << ") with side " << side << std::endl;
+    }
+}
 
 int main()
 {
