00d4694be7aacde9fd0e7c7266b8444b7186967c
Álvaro Bátrez <alvaro.batrez@machinecare.com.mx>
2024-09-03 16:21:56 -0600

    singleton fixed

--- creational_patterns/singleton/main.cpp
+++ creational_patterns/singleton/main.cpp
@@ -4,20 +4,20 @@
 class Singleton
 {
     private:
-
+    // Constructor privado para evitar instaciación directa
     Singleton()
     {
         std::cout << "Singleton instance created\n";
     }
 
+    // Destructor también puede ser privado
     ~Singleton()
     {
         std::cout << "Singleton instance destroyed\n";
     }
 
     public:
-
-    // Método estático para obtener la instancia única
+    // Método estático para obtener instancia única
     static Singleton &getInstance()
     {
         // Instancia se crea la primera vez que se llama este método
@@ -31,21 +31,24 @@ class Singleton
         std::cout << "Singleton instance is doing something\n";
     }
 
-    // Eliminar los métodos de copia para asegurar que la instancia no se copie
+    // Eliminar los métodos copia para asegurar que la instancia no se copie
     Singleton(const Singleton &) = delete;
     Singleton& operator=(const Singleton &) = delete;
 };
 
 int main()
 {
+    // Intento de obtener la instancia única de Singleton
     Singleton &instance1 = Singleton::getInstance();
     instance1.doSomething();
 
+    // Intento de obtener la misma instancia
     Singleton &instance2 = Singleton::getInstance();
     instance2.doSomething();
 
+    // Verificación de que ambas referencias son la misma instancia
     std::cout << "Are both instances the same? " << 
     std::boolalpha << static_cast<bool>(&instance1 == &instance2) << std::endl;
-    
+
     return 0;
 }
--- creational_patterns/singleton/main.py
+++ creational_patterns/singleton/main.py
@@ -6,18 +6,21 @@ class Singleton:
         if not cls._instance:
             cls._instance = super(Singleton, cls).__new__(cls, *args, **kwargs)
             print('Singleton instance created')
-        
+
         return cls._instance
     
     def do_something(self):
-        print("Singleton instance is doing something")
+        print('Singleton instance is doing something')
 
+# Cliente
 if __name__ == '__main__':
-
+    # Intento de crear la primera instancia
     instance1 = Singleton()
     instance1.do_something()
 
+    # Intento de crear otra instancia
     instance2 = Singleton()
     instance2.do_something()
 
-    print(f'Are both instances the same? {instance1 is instance2}')
+    # Verificación de que ambas referencias son la misma instancia
+    print('Are both instances the same? ', instance1 is instance2)
